d67ecf670a71a482e56201386a02d30d8d4a08a8
陈少凡 <394501487@qq.com>
2017-10-17 13:42:46 +0800

    删除域名http

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 var replace = function(html){
 	return html
-	.replace(/<\?=RIJI_SEEDIT_COM\?>/g,'http://riji.bozhong.com')
-	.replace(/<\?=UC_SEEDIT_COM\?>/g,'http://uc.bozhong.com')
-	.replace(/<\?=BBS_SEEDIT_COM\?>/g,'http://bbs.bozhong.com')
-	.replace(/<\?=I_SEEDIT_COM\?>/g,'http://i.bozhong.com')
-	.replace(/<\?=HUODONG_SEEDIT_COM\?>/g,'http://huodong.bozhong.com')
-	.replace(/<\?=WWW_SEEDIT_COM\?>/g,'http://www.bozhong.com')
+	.replace(/<\?=RIJI_SEEDIT_COM\?>/g,'//riji.bozhong.com')
+	.replace(/<\?=UC_SEEDIT_COM\?>/g,'//uc.bozhong.com')
+	.replace(/<\?=BBS_SEEDIT_COM\?>/g,'//bbs.bozhong.com')
+	.replace(/<\?=I_SEEDIT_COM\?>/g,'//i.bozhong.com')
+	.replace(/<\?=HUODONG_SEEDIT_COM\?>/g,'//huodong.bozhong.com')
+	.replace(/<\?=WWW_SEEDIT_COM\?>/g,'//www.bozhong.com')
 };
 exports.tpl = replace(require('./nav.tpl'));
 
